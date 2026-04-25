f0a3185a0ac6b0e88423e1fad7cc183f087f4d05
hsmtDP-T <ittuyo.hashi244@gmail.com>
2026-04-25 23:25:06 +0900

    設定新規作成・変更API作成　postman確認済み

--- back/app/main.py
+++ back/app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from app.presentation.user_router import router as user_router
-from app.infrastructure.db import get_db
+from app.presentation.setting_router import router as setting_router
 
 app = FastAPI()
 
-db = get_db
-
 app.include_router(user_router)
+app.include_router(setting_router)
